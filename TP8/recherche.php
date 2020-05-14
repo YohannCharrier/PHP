42a736c6aceaadfd6cbecb76e5538da0c95deeeb
YohannCharrier <60933713+YohannCharrier@users.noreply.github.com>
2020-05-14 10:33:49 +0200

    up

--- TP8/recherche.php
+++ TP8/recherche.php
@@ -10,50 +10,66 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="citation.php">Information</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="recherche.php">Recherche<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item ">
-                    <a class="nav-link" href="modification.php">Modification</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <h1>Rechercher une citation</h1>
-    <form>
-        <div class="col-md-4">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Auteur</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="citation.php">Information</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="recherche.php">Recherche<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="modification.php">Modification</a>
+                    </li>
+                </ul>
             </div>
+        </nav>
+        <h1>Rechercher une citation</h1>
+        <?php
 
-            <div class="form-group">
-                <label for="exampleInputPassword1">Siècle</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
+        include 'connexpdo.php';
+
+        $dsn = 'pgsql:host=127.0.0.1;port=5432;dbname=citations';
+        $user = 'postgres';
+        $password = 'pass';
+
+        $dbh = connexpdo($dsn,$user,$password);
+
+        $query1 = "SELECT nom, prenom FROM auteur";
+        $result1= $dbh->query($query1);
+
+        $query2 = "SELECT numero FROM siecle";
+        $result2 = $dbh->query($query2);
+
+        echo "<form action='recherche.php' method='post'>
+                <div class='col-md-4'>
+                    <div class='form-group'>
+                        <label for='exampleInputEmail1'>Auteur</label>
+                        <select class='form-control' name='author'>";
+        foreach ($result1 as $author){
+            echo "<option>".$author['prenom']." ".$author['nom']."</option>";
+        }
+        echo "</select>
+                </div>
+    
+                <div class='form-group'>
+                    <label for='exampleInputPassword1'>Siècle</label>
+                    <select class='form-control' name='siecle'>";
+        foreach ($result2 as $siecle){
+            echo "<option>".$siecle['numero']."</option>";
+        }
+        echo "</select>
+                </div>
+    
+                <button type='submit' class='btn btn-primary'>Rechercher</button>
             </div>
+        </form>";
+        if(isset($_POST["auteur"]) && isset[$_POST["siecle"]]){
 
-            <button type="submit" class="btn btn-primary">Rechercher</button>
-        </div>
-    </form>
+        }
+        ?>
+    </body>
+</html>
 
-
-    <?php
